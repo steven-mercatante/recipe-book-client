5ee01e0e3af9be5442c516bb785d9fd7629ffc5e
Steven Mercatante <steven.mercatante@gmail.com>
2021-12-31 12:45:07 -0500

    add meta viewport tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import Nav from "components/Nav";
-import "../styles/globals.css";
+import "styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { user } = pageProps;
 
   return (
-    <UserProvider user={user}>
-      <Nav />
-      <Component {...pageProps} />
-    </UserProvider>
+    <>
+      <Head>
+        <title>Recipes</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <UserProvider user={user}>
+        <Nav />
+        <Component {...pageProps} />
+      </UserProvider>
+    </>
   );
 }
