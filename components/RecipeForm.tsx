de7f0e7da4f6592c106d8e984d7f595a5a14b7fb
Steven Mercatante <steven.mercatante@gmail.com>
2022-01-08 23:49:32 -0500

    wire up Delete Recipe button

--- components/RecipeForm.tsx
+++ components/RecipeForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { recipesApi } from "../api";
 import { Form, Formik } from "formik";
 import TextField from "./TextField";
@@ -5,6 +6,7 @@ import { Recipe } from "recipe-book-api-client";
 import TextareaField from "./TextareaField";
 import { useRouter } from "next/router";
 import { Routes } from "../constants";
+import DeleteRecipeDialog from "./DeleteRecipeDialog";
 
 interface Props {
   recipe?: Recipe;
@@ -17,6 +19,7 @@ interface SubmitValues {
 }
 
 export default function RecipeForm({ recipe }: Props) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
   const router = useRouter();
 
   const showDeleteBtn = router.pathname === Routes.EditRecipe;
@@ -37,6 +40,16 @@ export default function RecipeForm({ recipe }: Props) {
     }
   }
 
+  function handleDeleteCancel() {
+    setShowDeleteDialog(false);
+  }
+
+  async function handleDeleteConfirm() {
+    setShowDeleteDialog(false);
+    await recipesApi.destroyRecipe(recipe?.id!);
+    router.push(Routes.ViewRecipes);
+  }
+
   return (
     <div className="recipe-form">
       <Formik
@@ -54,9 +67,18 @@ export default function RecipeForm({ recipe }: Props) {
         </Form>
       </Formik>
       {showDeleteBtn && (
-        <button className="bg-red-700 rounded-md px-6 py-2 text-white">
+        <button
+          className="bg-red-700 rounded-md px-6 py-2 text-white"
+          onClick={() => setShowDeleteDialog(true)}
+        >
           Delete Recipe
         </button>
+      )}
+      {showDeleteDialog && (
+        <DeleteRecipeDialog
+          onCancel={handleDeleteCancel}
+          onConfirm={handleDeleteConfirm}
+        />
       )}
     </div>
   );
