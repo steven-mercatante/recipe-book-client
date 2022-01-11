9df04a64f15d4f1a92189a5264d9458a25b8f61d
Steven Mercatante <steven.mercatante@gmail.com>
2022-01-11 15:21:24 -0500

    add Save button to RecipeForm

--- components/RecipeForm.tsx
+++ components/RecipeForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { recipesApi } from "../api";
 import { Form, Formik } from "formik";
 import TextField from "./TextField";
@@ -24,19 +24,22 @@ export default function RecipeForm({ recipe }: Props) {
 
   const showDeleteBtn = router.pathname === Routes.EditRecipe;
 
-  function handleSubmit(values: SubmitValues) {
+  async function handleSubmit(values: SubmitValues) {
     if (recipe?.id) {
-      recipesApi.updateRecipe(recipe?.id!, {
+      await recipesApi.updateRecipe(recipe?.id!, {
         name: values.name,
         ingredients: values.ingredients,
         instructions: values.instructions,
       });
+      await router.push(Routes.ViewRecipe.replace("[id]", recipe.id));
     } else {
-      recipesApi.createRecipe({
+      const resp = await recipesApi.createRecipe({
         name: values.name,
         ingredients: values.ingredients,
         instructions: values.instructions,
       });
+      // @ts-ignore
+      await router.push(Routes.ViewRecipe.replace("[id]", resp.data.id));
     }
   }
 
@@ -64,16 +67,23 @@ export default function RecipeForm({ recipe }: Props) {
           <TextField name="name" label="Name" />
           <TextareaField name="ingredients" label="Ingredients" />
           <TextareaField name="instructions" label="Instructions" />
+          {showDeleteBtn && (
+            <button
+              className="bg-red-700 rounded-md px-6 py-2 text-white mr-4"
+              onClick={() => setShowDeleteDialog(true)}
+            >
+              Delete Recipe
+            </button>
+          )}
+          <button
+            className="invisible lg:visible px-6 py-2 bg-green-300 text-green-900 rounded-md"
+            type="submit"
+          >
+            <strong>Save</strong>
+          </button>
         </Form>
       </Formik>
-      {showDeleteBtn && (
-        <button
-          className="bg-red-700 rounded-md px-6 py-2 text-white"
-          onClick={() => setShowDeleteDialog(true)}
-        >
-          Delete Recipe
-        </button>
-      )}
+
       {showDeleteDialog && (
         <DeleteRecipeDialog
           onCancel={handleDeleteCancel}
