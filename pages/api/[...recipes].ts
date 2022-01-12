dd8bfa08b078b9cd297711660f413bd7468898b4
Steven Mercatante <steven.mercatante@gmail.com>
2022-01-12 10:11:24 -0500

    remove session auth on [...recipes] catch all route

--- pages/api/[...recipes].ts
+++ pages/api/[...recipes].ts
@@ -1,33 +1,39 @@
-/**
- * This catch all route handles all Recipe endpoints _except_
- * GETing a Recipe to view it. See /api/recipes/[slug].ts for that.
- */
 import httpProxyMiddleware from "next-http-proxy-middleware";
 import { NextApiRequest, NextApiResponse } from "next";
-import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { getAccessToken } from "@auth0/nextjs-auth0";
+
+const proxyConfig = {
+  target: process.env.API_URL,
+  pathRewrite: [
+    {
+      // @ts-ignore
+      patternStr: /^\/api\/recipes\/(.*)/,
+      // @ts-ignore
+      replaceStr: /\/recipes\/$1/,
+    },
+    {
+      // @ts-ignore
+      patternStr: /^\/api\/recipes/,
+      // @ts-ignore
+      replaceStr: /\/recipes\//,
+    },
+  ],
+};
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const accessTokenResp = await getAccessToken(req, res, {});
-  return httpProxyMiddleware(req, res, {
-    target: process.env.API_URL,
-    headers: {
+  try {
+    /**
+     * Auth isn't required when viewing a recipe.
+     */
+    const accessTokenResp = await getAccessToken(req, res, {});
+    proxyConfig.headers = {
       Authorization: `Bearer ${accessTokenResp.accessToken}`,
-    },
-    pathRewrite: [
-      {
-        // @ts-ignore
-        patternStr: /^\/api\/recipes\/(.*)/,
-        // @ts-ignore
-        replaceStr: /\/recipes\/$1/,
-      },
-      {
-        // @ts-ignore
-        patternStr: /^\/api\/recipes/,
-        // @ts-ignore
-        replaceStr: /\/recipes\//,
-      },
-    ],
-  });
+    };
+  } catch (e) {
+    // TODO: do something??
+  }
+  // @ts-ignore
+  return httpProxyMiddleware(req, res, proxyConfig);
 }
 
-export default withApiAuthRequired(handler);
+export default handler;
